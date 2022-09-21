2081734aa5ae9254e6e74546dd4e1a33c1453b64
Nicolae Pop <nicupop729@gmail.com>
2022-09-20 20:10:28 -0400

    Update the Input component

--- src/components/movie/Input.jsx
+++ src/components/movie/Input.jsx
@@ -2,11 +2,15 @@
 /* eslint-disable object-curly-newline */
 import React from 'react';
 
-const Input = ({ type, id, className }) => (
+const Input = ({ type, id, className, name, value, onChange }) => (
   <input
     type={type}
     id={id}
     className={className}
+    name={name}
+    value={value}
+    onChange={onChange}
+    required
   />
 
 );
