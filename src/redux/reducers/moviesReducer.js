5cd3211a2f73cea4d478e1332aa3f77459887dbb
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2022-09-22 14:45:38 +0300

    Add reducers

--- src/redux/reducers/moviesReducer.js
+++ src/redux/reducers/moviesReducer.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const FETCH_MOVIES = 'FETCH_MOVIES';
+const ADD_MOVIE = 'ADD_MOVIE';
+const DELETE_MOVIE = 'DELETE_MOVIE';
 
 // App initial state
 const initialState = {
@@ -13,6 +15,15 @@ export const movies = (payload) => ({
   type: FETCH_MOVIES,
   payload,
 });
+export const addMovie = (payload) => ({
+  type: ADD_MOVIE,
+  payload,
+});
+
+export const deleteMovie = (payload) => ({
+  type: DELETE_MOVIE,
+  payload,
+});
 
 export const fetchMovies = () => async (dispatch) => {
   const res = await axios.get('http://localhost:3000/api/v1/movies');
@@ -20,11 +31,35 @@ export const fetchMovies = () => async (dispatch) => {
   dispatch(movies(newMovies));
 };
 
+export const addMovies = async (movieData, dispatch) => {
+  const sendMovie = {
+    title: movieData.Title,
+    description: movieData.Description,
+    photo: movieData.Photo,
+    director: movieData.Director,
+    playing_time: movieData['Playing Time'],
+    release_date: movieData['Release Date'],
+    genre: movieData.Genre,
+    ticket_price: movieData['Ticket Price'],
+    country: movieData.Country,
+    lead_cast: movieData['Lead Cast'],
+  };
+  const response = await axios.post(
+    'http://localhost:3000/api/v1/movies/create',
+    sendMovie,
+  );
+  dispatch(addMovie(response.data.movie));
+};
+
 // Reducer
 const moviesReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_MOVIES:
       return { movies: [...action.payload] };
+    case ADD_MOVIE:
+      return { movies: [...movies, action.payload] };
+    case DELETE_MOVIE:
+      return state.movies.filter((movie) => movie.id !== action.payload);
     default:
       return state;
   }
